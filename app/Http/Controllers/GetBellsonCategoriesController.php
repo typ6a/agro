b3054923c2319fe6ee9049a013d2a12bf93e4e76
Dmitry Znak <znakdmitry@gmail.com>
2017-03-29 04:22:06 +0300

    bellsonData ready for csv write

--- app/Http/Controllers/GetBellsonCategoriesController.php
+++ app/Http/Controllers/GetBellsonCategoriesController.php
@@ -105,265 +105,52 @@ class GetBellsonCategoriesController extends Controller
                 $this->crawler = new Crawler($html);
                 $price       = trim($this->crawler->filter('.wrap-col .wrap-item-price')->text());
                 $price       = stristr($price, 'Цена');
-                $description = trim($this->crawler->filter('.right #tab-description')->text());
-                pre($description,);
-        }
 
- 		
- 	}
+                $price = str_replace ('Цена' , '' , $price);
+                $price = trim(str_replace (' грн.' , '' , $price));
+                $description = trim($this->crawler->filter('.right #tab-description p')->siblings()->text());
+                
+
+                $properties = [];
+		        $propertyRows = $this->crawler->filter('#tab-attribute .attribute tbody tr');
+		        // pre($propertyRows,1);
+
+		        if ($propertyRows->count()) {
+		            $propertyRows->each(function (Crawler $propertyRow) use (&$properties) {
+		                $propertyName = trim($propertyRow->filter('td:first-child')->text());
+		                $propertyValue = trim($propertyRow->filter('td:last-child')->text());   
+		                $properties[] = [
+								'propertyName'  => $propertyName,
+								'propertyValue' => $propertyValue,
+		                    ];
+		                
+		            });
+		        }
+		        $product['price'] = $price;
+		        $product['properties'] = $properties;
+
+		        $image = [];
+		        $imageUrl = $this->crawler->filter('.product-info .left .image a')->attr('href');
+		        $imageFileName = $product['name'];
+		        $imageFilePath = '../storage/imagesBellson/' . md5($imageFileName) . '.jpg';
+		        if(!file_exists($imageFileName)){
+		        	file_put_contents($imageFilePath, file_get_contents($imageUrl));
+		        }
+		        $product['image'] = [
+		        	'imageUrl' => $imageUrl,
+		        	'imageFile' => $imageFilePath,
+		        ];
+		        // pre($image);
+                // pre($imageUrl);
+                pre($product);
+                // pre($product['url'] . $description . '<hr>');
+        }
+		       // pre($products);
         
         //pre($propertyAddress, 1);
     
 
-   
-    protected function parsePropertySummary()
-    {
-        $propertySummary = [];
-        //$propertyExtra = null;
-        $propertyBeds = null;
-        $propertyBedsItems = $this->crawler->filter('.summarybar.summarybar--property .col-xs-3.summarybar-item span')->eq(0);
-        if ($propertyBedsItems->count()) {
-            foreach ($propertyBedsItems as $propertyBeds) {
-                $propertyBeds = $propertyBeds->nodeValue;
-            }
-            $propertyBeds = trim($propertyBeds);
-        }
-
-        $propertyBaths = null;
-        $propertyBathsItems = $this->crawler->filter('.summarybar.summarybar--property .col-xs-3.summarybar-item span')->eq(1);
-        if ($propertyBathsItems->count()) {
-            foreach ($propertyBathsItems as $propertyBaths) {
-                $propertyBaths = $propertyBaths->nodeValue;
-            }
-            $propertyBaths = trim($propertyBaths);
-        }
-
-        $propertySize = null;
-        $propertySizeItems = $this->crawler->filter('.summarybar.summarybar--property .col-xs-3.summarybar-item span')->eq(2);
-        if ($propertySizeItems->count()) {
-            foreach ($propertySizeItems as $propertySize) {
-                $propertySize = $propertySize->nodeValue;
-            }
-            $propertySize = trim($propertySize);
-        }
-
-        $propertyType = null;
-        $propertyTypeItems = $this->crawler->filter('.summarybar.summarybar--property .col-xs-3.summarybar-item span')->eq(3);
-        if ($propertyTypeItems->count()) {
-            foreach ($propertyTypeItems as $propertyType) {
-                $propertyType = $propertyType->nodeValue;
-            }
-            $propertyType = trim($propertyType);
-        }
-
-        $propertySummary = [
-            'beds' => $propertyBeds,
-            'baths' => $propertyBaths,
-            'size' => $propertySize,
-            'type' => $propertyType,
-        ];
-        //pre($propertySummary,1);
-        return $propertySummary;
-    }
+   }
+    
 
 
-    protected function parsePropertyListingID()
-    {
-        $propertyListingIDs = $this->crawler->filter('.propertyheader-secondary.propertyheader-piped_list li')->last();
-        foreach ($propertyListingIDs as $propertyListingID) {
-            $propertyListingID = $propertyListingID->nodeValue;
-        }
-        $propertyListingID = str_replace('Listing ID:', '', preg_replace('|\s+|', ' ', trim($propertyListingID)));
-        return $propertyListingID;
-    }
-
-
-    protected function parsePropertyDescription()
-    {
-        $description = [];
-        $propertyDescriptionTitles = $this->crawler->filter('.propertydetails-description_title');
-        foreach ($propertyDescriptionTitles as $propertyDescriptionTitle) {
-            $propertyDescriptionTitle = $propertyDescriptionTitle->nodeValue;
-        }
-        $propertyDescriptionTitle = trim($propertyDescriptionTitle);
-
-        $propertyDescriptions = $this->crawler->filter('.propertydetails-description div[itemprop="description"]');
-        foreach ($propertyDescriptions as $propertyDescription) {
-            $propertyDescription = $propertyDescription->nodeValue;
-        }
-        $propertyDescription = trim($propertyDescription);
-        $description = [
-            'propertyDescriptionTitle' => $propertyDescriptionTitle,
-            'propertyDescription' => $propertyDescription
-        ];
-
-        return $description;
-
-    }
-
-
-    protected function parsePropertyPrice()
-    {
-        $items = $this->crawler->filter('.propertyheader-price');
-        $price = null;
-        foreach ($items as $price) {
-            $price = $price->nodeValue;
-        }
-        $price = trim(preg_replace('/[^\d]/', '', $price));
-        //pre($price, 1);
-        return $price;
-    }
-
-
-    protected function parsePropertyOverview()
-    {
-        $features = [];
-        $featureRows = $this->crawler->filter('.contenttable tbody tr');
-        if ($featureRows->count()) {
-            $featureRows->each(function (Crawler $featureRow) use (&$features) {
-                $featureName = null;
-                $featureValue = null;
-                $featureNameEl = $featureRow->filter('th');
-                if ($featureNameEl->count()) {
-                    $featureName = trim($featureNameEl->text());
-                }
-                $featureValueEl = $featureRow->filter('td');
-                if ($featureValueEl->count()) {
-                    $featureValue = trim($featureValueEl->text());
-                    //pre($featureValue);
-                }
-                if ($featureName && $featureValue) {
-                    $features[] = [
-                        'name' => $featureName,
-                        'value' => $featureValue
-                    ];
-                }
-            });
-
-        }
-        return $features;
-    }
-
-    protected function parsePropertyImages()
-    {
-        $images = [];
-        $items = $this->crawler->filter('.galleria-thumbnails-container .galleria-thumbnails-list .galleria-thumbnails .galleria-image img');
-        //pre($items->count(),1);
-        if ($items->count()) {
-            $items->each(function (Crawler $image) {
-                $filename = null;
-                $url = $image->attr('src');
-                $imageUrl = 'http://' . trim($url, '/');
-                $filename = md5($url) . '.jpg';
-                $images[] = [
-                    'url' => $imageUrl,
-                    'filename' => $filename
-                ];
-                //pre($images->url,1);
-                $filepath = 'd:\workspace\crep\public\data\images\\' . $filename;
-                $data = file_get_contents($url);
-                file_put_contents($filepath, $data);
-            });
-        }
-        //exit('adsad');
-        return $images;
-    }
-
-    protected function parsePropertyPictures()
-    {
-        $images = [];
-        $items = $this->crawler->filter('.galleria-thumbnails-container  img');// do not works!!!!!!!!!!!!suka
-        //pre($items->count(),1);
-        if ($items->count()) {
-            $items->each(function (Crawler $image) {
-                $filename = null;
-                $url = $image->attr('src');
-                $imageUrl = 'http://' . trim($url, '/');
-                $filename = md5($url) . '.jpg';
-                $images[] = [
-                    'url' => $imageUrl,
-                    'filename' => $filename
-                ];
-                //pre($images->url,1);
-                $filepath = 'd:\workspace\crep\public\data\images\\' . $filename;
-                $data = file_get_contents($url);
-                file_put_contents($filepath, $data);
-            });
-        }
-        //exit('adsad');
-        return $images;
-    }
-
-
-
-    protected function parsePropertyRealtors()
-    {
-        $realtorInfo = [];
-        $realtorCells = $this->crawler->filter('.box--contact .row.contact');
-        //pre($realtorCells->count(),1);
-        if ($realtorCells->count()) {
-            $realtorCells->each(function (Crawler $realtorCell) use (&$realtorInfo) {
-
-                $images =[];
-                $realtorPhones=[];
-
-                $realtorPicture = $realtorCell->filter('.contact-logos img');
-                //pre ($realtorPicture->count(), 1);
-                if ($realtorPicture->count()) {
-
-                        $filename = null;
-                        $url = $realtorPicture->attr('src');
-                        $imageUrl = 'http://' . trim($url, '/');
-                        $filename = md5($url) . '.jpg';
-                        $images[] = [
-                            'url' => $imageUrl,
-                            'filename' => $filename,
-                        ];
-                        //pre($images->url,1);
-                        $filepath = 'd:\workspace\crep\public\data\images\\' . $filename;
-                        $data = file_get_contents($url);
-                        file_put_contents($filepath, $data);
-
-                }
-
-
-
-                $realtorOfficeTitleEl = $realtorCell->filter('.contact-office_name');
-                if ($realtorOfficeTitleEl->count()) {
-                    $realtorOfficeTitle = trim($realtorOfficeTitleEl->text());
-                }
-
-
-                $realtorCellPhones = $realtorCell->filter('.contact-phonenumbers dd > a');
-                //pre($realtorCellPhones->count(),1);
-                if ($realtorCellPhones->count()) {
-                    $realtorCellPhones->each(function (Crawler $span) use (&$realtorPhones) {
-                        $realtorPhones[] = trim($span->text());
-                    });
-                }
-
-
-
-                $realtorNameEl = $realtorCell->filter('.contact-agent_name > a');
-                if ($realtorNameEl->count()) {
-                    $realtorName = trim($realtorNameEl->text());
-                }
-
-                $realtorInfo[] = [
-                    'images' => $images,
-                    'realtorOfficeTitle' => $realtorOfficeTitle,
-                    'realtorPhones' => $realtorPhones,
-                    'realtorName' => $realtorName,
-
-
-                ];
-
-            });
-
-        }
-        pre($realtorInfo,1);
-        return $realtorInfo;
-    }
-
-
-}
